ddd9bbcd2604b71b1889941492ab887869b903f6
luqman <putraian2005@gmail.com>
2022-07-08 11:20:01 +0700

    mengubah isi file index.php

--- index.php
+++ index.php
@@ -9,5 +9,11 @@
 <body>
     <h1>HELLO WORD!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia sequi commodi pariatur minima doloribus. Molestias voluptate debitis beatae repellendus nemo nihil quia! Vel soluta praesentium tenetur necessitatibus laborum cum quam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+        <li>empat</li>
+    </ul>
 </body>
 </html>
